7400f00e1cc5f3710549bbb23d86a86d53a21845
Qingshan <qs@qingshan.dev>
2022-10-01 23:07:28 -0700

    Add more fish command aliases

--- fish/config.fish
+++ fish/config.fish
@@ -24,13 +24,18 @@ function ide
   tmux split-window -h -p 50
 end
 
+function mkcd
+  mkdir -p -- "$1" && cd -P -- "$1"
+end
+
 # theme
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme gruvbox
 set -g theme_date_format "+%H:%M:%S"
+set -g theme_display_git no
 
 # abbr
-abbr --add e "exit"
+abbr --add e 'exit'
 
 # git
 abbr --add g 'git'
@@ -43,38 +48,50 @@ abbr --add gp 'git push origin'
 abbr --add gs 'git status'
 abbr --add glf 'git logf'
 abbr --add glo 'git logo'
-abbr --add gl "git pull"
-abbr --add gp "git push"
-abbr --add lg "lazygit"
+abbr --add gl 'git pull'
+abbr --add gp 'git push'
+abbr --add lg 'lazygit'
 
 # lsd
 if command -sq lsd
-  abbr --add l "lsd --group-dirs first -A"
-  abbr --add ll "lsd --group-dirs first -Al"
-  abbr --add lt "lsd --group-dirs last -A --tree"
+  abbr --add l 'lsd --group-dirs first -A'
+  abbr --add ll 'lsd --group-dirs first -Al'
+  abbr --add lt 'lsd --group-dirs last -A --tree'
 end
-abbr --add rmr "rm -rf"
+abbr --add rmr 'rm -rf'
 
 # brew
-abbr --add bi "brew install"
-abbr --add bic "brew install --cask"
-abbr --add bo "brew outdated"
-abbr --add bu "brew update"
+abbr --add bi 'brew install'
+abbr --add bic 'brew install --cask'
+abbr --add bo 'brew outdated'
+abbr --add bu 'brew update'
 
 # config
-abbr --add sf "source ~/.config/fish/config.fish"
-abbr --add st "tmux source ~/.tmux.conf"
+abbr --add sf 'source ~/.config/fish/config.fish'
+abbr --add st 'tmux source ~/.tmux.conf'
 
 # vim
-abbr --add v "vim (fd --type f --hidden --follow --exclude .git | fzf-tmux -p --reverse)"
-abbr --add va "vim ~/.config/alacritty/alacritty.yml"
-abbr --add vf "vim ~/.config/fish/config.fish"
-abbr --add vpc "nvim +PlugClean"
-abbr --add vpi "nvim +PlugInstall"
-abbr --add vpu "nvim +PlugUpdate"
+abbr --add v 'vim (fd --type f --hidden --follow --exclude .git | fzf-tmux -p --reverse)'
+abbr --add va 'vim ~/.config/alacritty/alacritty.yml'
+abbr --add vb 'vim ~/.dotfiles/macos/Brewfile'
+abbr --add vf 'vim ~/.config/fish/config.fish'
+abbr --add vs 'vim ~/.ssh/config'
+abbr --add vt 'vim ~/.dotfiles/.tmux.conf'
+abbr --add vv 'vim ~/.dotfiles/.vimrc'
+abbr --add vpc 'vim +PlugClean'
+abbr --add vpi 'vim +PlugInstall'
+abbr --add vpu 'vim +PlugUpdate'
 
 # rsync
 abbr --add rcp "rsync -vhra --include='**.gitignore' --exclude='/.git' --filter=':- .gitignore' --delete-after"
+abbr --add rscp 'rsync --archive --compress-level=3 --copy-links --partial --inplace --progress --rsh=ssh -r'
+
+# yt-dlp
+abbr --add ytdl 'yt-dlp --write-auto-sub --sub-lang en --convert-subs=srt'
+
+# aliases
+alias ql='qlmanage -p 2>/dev/null'
+alias sucs='sort | uniq -c | sort -n'
 
 # fzf
 if command -v fzf &> /dev/null
