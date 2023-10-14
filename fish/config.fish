a6d0db1ce7c29a35b236a79ea49399c988be02ab
Qingshan <qs@qingshan.dev>
2023-10-13 22:56:57 -0700

    fish: config refactor

--- fish/config.fish
+++ fish/config.fish
@@ -10,19 +10,19 @@ set -gx PROJECTS ~/code
 fish_add_path -m ~/.bin
 fish_add_path -m ~/.dotfiles/bin
 fish_add_path -m ~/.local/bin
-if test -d "$HOME/.go/bin"
+if test -d ~/.go/bin
   fish_add_path -m ~/.go/bin
 end
-if test -d "$HOME/.cargo/bin"
+if test -d ~/.cargo/bin
   fish_add_path -m ~/.cargo/bin
 end
-if test -d "/usr/local/opt/llvm/bin"
+if test -d /usr/local/opt/llvm/bin
   fish_add_path -m /usr/local/opt/llvm/bin
 end
-if test -d "$HOME/Library/Application\ Support/multipass/bin"
+if test -d ~/Library/Application\ Support/multipass/bin
   fish_add_path -m ~/Library/Application\ Support/multipass/bin
 end
-if test -d "/Applications/IntelliJ IDEA CE.app/Contents/MacOS"
+if test -d /Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS
   fish_add_path -m /Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS
 end
 fish_add_path -m /opt/local/bin
@@ -91,18 +91,25 @@ abbr --add rmr 'rm -rf'
 
 # git
 abbr --add g 'git'
-abbr --add gbr 'git branch'
+abbr --add ga 'git add'
+abbr --add gaa 'git add -A'
+abbr --add gb 'git branch'
+abbr --add gbd 'git branch -d'
+abbr --add gbD 'git branch -D'
+abbr --add gc 'git commit'
 abbr --add gcm 'git commit -m'
 abbr --add gca 'git commit --amend -m'
 abbr --add gco 'git checkout'
+abbr --add gcb 'git checkout -b'
 abbr --add gd 'git diff -w'
 abbr --add gds 'git diff -w --staged'
-abbr --add gp 'git push origin'
 abbr --add gs 'git status'
 abbr --add glf 'git logf'
 abbr --add glo 'git logo'
-abbr --add gl 'git pull'
-abbr --add gp 'git push'
+abbr --add gp 'git pull'
+abbr --add gpr 'git pull --rebase'
+abbr --add gP 'git push'
+abbr --add gPo 'git push origin'
 
 # brew
 abbr --add bi 'brew install'
@@ -133,7 +140,7 @@ abbr --add vpi 'vim +PlugInstall'
 abbr --add vpu 'vim +PlugUpdate'
 
 # rsync
-abbr --add rcp "rsync -vhra --include='**.gitignore' --exclude='/.git' --filter=':- .gitignore' --delete-after"
+abbr --add rcp 'rsync -vhra --include="**.gitignore" --exclude="/.git" --filter=":- .gitignore" --delete-after'
 abbr --add rscp 'rsync --archive --compress-level=3 --copy-links --partial --inplace --progress --rsh=ssh -r'
 abbr --add rcpl 'rsync --compress --verbose --human-readable --partial --progress'
 
@@ -151,66 +158,95 @@ abbr --add lab 'ssh lab'
 abbr --add homelab 'ssh homelab'
 
 # make
-abbr --add mb make build
-abbr --add mr make run
-abbr --add mt make test
-abbr --add mc make clean
+abbr --add mb 'make build'
+abbr --add mr 'make run'
+abbr --add mt 'make test'
+abbr --add mc 'make clean'
 
 # just
 if command -q just
-  abbr --add jb just build
-  abbr --add jr just run
-  abbr --add jt just test
+  abbr --add jb 'just build'
+  abbr --add jr 'just run'
+  abbr --add jt 'just test'
 end
 
 # rust
-abbr --add ca cargo add
-abbr --add cb cargo build
-abbr --add cba cargo build --all-targets
-abbr --add cbe cargo build --example
-abbr --add cbr cargo build --release
-abbr --add cr cargo run
-abbr --add cre cargo run --example
-abbr --add crr cargo run --release
-abbr --add ct cargo test
-abbr --add cta cargo test --all-targets
-abbr --add ctt cargo test --test
-abbr --add ch cargo check
-abbr --add ci cargo install
-abbr --add cl cargo clippy
-abbr --add cs cargo search
-abbr --add cw cargo watch -x 'run'
-abbr --add cdoc cargo doc
+abbr --add ca 'cargo add'
+abbr --add cb 'cargo build'
+abbr --add cba 'cargo build --all-targets'
+abbr --add cbe 'cargo build --example'
+abbr --add cbr 'cargo build --release'
+abbr --add cr 'cargo run'
+abbr --add cre 'cargo run --example'
+abbr --add crr 'cargo run --release'
+abbr --add ct 'cargo test'
+abbr --add cta 'cargo test --all-targets'
+abbr --add ctt 'cargo test --test'
+abbr --add ch 'cargo check'
+abbr --add ci 'cargo install'
+abbr --add cl 'cargo clippy'
+abbr --add cs 'cargo search'
+abbr --add cw 'cargo watch -x "run"'
+abbr --add cdoc 'cargo doc'
+
+# Docker
+abbr -a de 'docker exec'
+abbr -a deit 'docker exec -it'
+abbr -a dka 'docker kill (docker ps -q)'
+abbr -a dil 'docker image list'
+abbr -a dip 'docker image prune'
+abbr -a dipa 'docker image prune -a'
+abbr -a dlg 'docker logs'
+abbr -a dlgf 'docker logs -f'
+abbr -a dps 'docker ps'
+abbr -a dpsa 'docker ps -a'
+abbr -a drm 'docker rm'
+abbr -a drmi 'docker rmi'
+abbr -a drun 'docker run'
+abbr -a drunn 'docker run --name'
+abbr -a dsa 'docker stop (docker ps -q)'
+# Docker Compose
+abbr -a dcd 'docker compose down'
+abbr -a dck 'docker compose kill'
+abbr -a dcl 'docker compose logs'
+abbr -a dclf 'docker compose logs -f'
+abbr -a dclft 'docker compose logs -f --tail'
+abbr -a dclt 'docker compose logs --tail'
+abbr -a dcS 'docker compose stop'
+abbr -a dcs 'docker compose start'
+abbr -a dcu 'docker compose up'
+abbr -a dcud 'docker compose up -d'
+abbr -a dcuf 'docker compose up -f'
 
 # zk
 if command -q zk
-  abbr --add zc zk snippet
-  abbr --add zj zk journal
-  abbr --add zs zk scratch
-  abbr --add zn zk inbox
+  abbr --add zc 'zk snippet'
+  abbr --add zj 'zk journal'
+  abbr --add zs 'zk scratch'
+  abbr --add zn 'zk inbox'
 end
 
 # multipass
 if command -q multipass
-  abbr --add mp multipass
+  abbr --add mp 'multipass'
 end
 
 # aliases
 alias ql='qlmanage -p 2>/dev/null'
 alias sucs='sort | uniq -c | sort -n'
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
 alias gcurl='grpcurl -plaintext localhost:3000'
 
 # fzf
 if command -v fzf &> /dev/null
-  set -gx FZF_DEFAULT_OPTS "--layout=reverse --inline-info --height '80%' --select-1 --exit-0"
+  set -gx FZF_DEFAULT_OPTS '--layout=reverse --inline-info --height "80%" --select-1 --exit-0'
   if command -v bat &> /dev/null
     set -gx FZF_PREVIEW_FILE '--ansi --preview-window "right:60%" --preview "bat --color=always --style=header,grid "'
   end
   if command -v fd &> /dev/null
-    set -gx  FZF_DEFAULT_COMMAND "fd --hidden --follow --exclude .git --type f"
+    set -gx  FZF_DEFAULT_COMMAND 'fd --hidden --follow --exclude .git --type f'
   end
 end
 
