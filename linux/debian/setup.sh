c358305efa7fa70a09d41605e2a4a91dfdd1c7ec
Qingshan <qs@qingshan.dev>
2025-09-03 14:55:54 -0700

    setup for debian 13

---
+++ linux/debian/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y \
+  curl \
+  wget \
+  make \
+  git \
+  fish \
+  fzf \
+  unzip \
+  ripgrep \
+  fd-find \
+  bat \
+  direnv \
+  zoxide \
+  starship \
+  htop \
+  rsync \
+  tmux \
+  vim \
+  gh \
+  jq \
+  yq
