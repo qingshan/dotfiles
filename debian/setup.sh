5a22e9ec7044548c7f5a253caa82e0489eba54cd
Qingshan <qs@qingshan.dev>
2025-12-18 03:14:30 +0000

    debian add line break in script

--- debian/setup.sh
+++ debian/setup.sh
@@ -33,7 +33,7 @@ sudo apt install -y \
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip \
   && cd ~/.local/share/fonts \
   && unzip JetBrainsMono.zip \
-  && rm JetBrainsMono.zip
+  && rm JetBrainsMono.zip \
   && fc-cache -fv
 
 # starship
