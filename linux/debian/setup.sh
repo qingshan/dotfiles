4918a026534acfc3b0fdfb3ea06908389548168f
Qingshan <qs@qingshan.dev>
2026-09-03 15:49:08 -0700

    linux: setup for ubuntu

--- linux/debian/setup.sh
+++ linux/debian/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+fi
+
+if [[ "$ID" == "ubuntu" ]]; then
+  sudo apt-add-repository ppa:fish-shell/release-4
+fi
+
 sudo apt update
 sudo apt install -y \
   curl \
@@ -24,15 +32,12 @@ sudo apt install -y \
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo bash -
 sudo apt-get install -y nodejs
 
-if [ -f /etc/os-release ]; then
-  source /etc/os-release
-  if [[ "$ID" == "debian" ]]; then
-    sudo apt install -y starship fzf
-  elif [[ "$ID" == "ubuntu" ]]; then
-    curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
-    FZF_VERSION=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-    curl -sL "https://github.com/junegunn/fzf/releases/latest/download/fzf-$FZF_VERSION-linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin
-  else
-    echo "This system is neither Debian nor Ubuntu (ID: $ID)."
-  fi
+if [[ "$ID" == "debian" ]]; then
+  sudo apt install -y starship fzf
+elif [[ "$ID" == "ubuntu" ]]; then
+  curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
+  FZF_VERSION=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+  curl -sL "https://github.com/junegunn/fzf/releases/latest/download/fzf-$FZF_VERSION-linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin
+else
+  echo "This system is neither Debian nor Ubuntu (ID: $ID)."
 fi
