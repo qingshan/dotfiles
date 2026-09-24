fab5ba5d5b40520c60946805d7db40fbded8aeb5
Qingshan <qs@qingshan.dev>
2026-09-24 14:14:19 -0700

    linux: remove docker from redhat setup script

--- linux/redhat/setup.sh
+++ linux/redhat/setup.sh
@@ -11,14 +11,6 @@ sudo dnf install -y make \
 
 sudo dnf install 'dnf-command(config-manager)'
 
-# docker
-echo "Install docker"
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce -y
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-sudo usermod -aG docker $USER
-
 # fish
 echo "Install fish"
 FISH_VERSION=$(curl -s "https://api.github.com/repos/fish-shell/fish-shell/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
